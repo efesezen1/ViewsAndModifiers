f188706b913e28e613d399349ba5fae13107404f
Efe Sezen <efesezen1@gmail.com>
2024-12-23 17:24:55 +0300

    Initial Commit

---
+++ ViewsAndModifiers/ViewsAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndModifiersApp.swift
+//  ViewsAndModifiers
+//
+//  Created by Efe Sezen on 23.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ViewsAndModifiersTests/ViewsAndModifiersTests.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndModifiersTests.swift
+//  ViewsAndModifiersTests
+//
+//  Created by Efe Sezen on 23.12.2024.
+//
+
+import Testing
+@testable import ViewsAndModifiers
+
+struct ViewsAndModifiersTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
